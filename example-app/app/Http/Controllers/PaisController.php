f43ade0909ff6e0a6383be77df07d9ba8e354594
BrunoFhome <124011928+BrunoFhome@users.noreply.github.com>
2024-09-03 20:34:00 -0300

    php_laravel_projeto

---
+++ example-app/app/Http/Controllers/PaisController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PaisController extends Controller
+{
+    public function index(){
+        $paises = [
+            ['nome' => 'Brasil', 'capital' => 'Brasilia'],
+            ['nome' => 'Argentina', 'capital' => 'Buenos Aires'],
+            ['nome' => 'Chile', 'capital' => 'Santiago'],
+        ];
+
+        //retornar a view
+    }
+
+    public function show($nome){
+        
+        switch($nome){
+            case 'Brasil' : 
+                echo 'Brasilia';
+                break;
+            case 'Argentina':
+                echo 'Buenos Aires';
+                break;
+            case 'Chile':
+                echo 'Santiago';
+                break;
+        }
+
+        //retornar para view
+    }
+}
